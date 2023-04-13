4f4900ad7ce4421f2af5e8aa6516146d3a915c65
Robert <Roberttsax18@gmail.com>
2023-04-13 17:16:26 -0400

    All done

--- src/index.js
+++ src/index.js
@@ -40,3 +40,49 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const aList = document.querySelectorAll('a');
+console.log(aList)
+aList[0].textContent = 'Services';
+aList[1].textContent = 'Product';
+aList[2].textContent = 'Vision';
+aList[3].textContent = 'Features';
+aList[4].textContent = 'About';
+aList[5].textContent = 'Contact';
+aList[6].textContent = 'Copyright Great Idea! 2021';
+console.log(aList[5]);
+
+const imgList = document.querySelectorAll('img');
+console.log(imgList[0]);
+imgList[0].setAttribute('src', 'http://localhost:9000/img/logo.png');
+imgList[1].setAttribute('src', 'http://localhost:9000/img/cta.png');
+imgList[2].setAttribute('src', 'http://localhost:9000/img/accent.png');
+
+const h4s = document.querySelectorAll('h4');
+console.log(h4s);
+h4s[0].textContent = 'Features';
+h4s[1].textContent = 'About';
+h4s[2].textContent = 'Services';
+h4s[3].textContent = 'Product';
+h4s[4].textContent = 'Vision';
+h4s[5].textContent = 'Contact';
+
+const paras = document.querySelectorAll('p');
+console.log(paras);
+paras[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et.Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paras[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paras[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paras[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paras[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+paras[5].textContent = '123 Way 456 Street Somewhere, USA';
+paras[6].textContent = '1 (888) 888-8888';
+paras[7].textContent = 'sales@greatidea.io';
+
+
+const h1 =document.querySelector('h1');
+console.log(h1);
+h1.textContent = 'Dom Is Awesome';
+
+const buttons =document.querySelectorAll('button');
+console.log(buttons);
+buttons[0].textContent = 'Get Started';
